a52954396f7b605635c49eb858943fc954dc979d
Kahlil Nuñez <kah.nunez@duocuc.cl>
2023-04-13 19:36:17 -0400

    first-commit

---
+++ src/main/java/app/back/periferico/BackPerifericoApplication.java
@@ -0,0 +1,13 @@
+package app.back.periferico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackPerifericoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackPerifericoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/back/periferico/BackPerifericoApplicationTests.java
@@ -0,0 +1,13 @@
+package app.back.periferico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackPerifericoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
